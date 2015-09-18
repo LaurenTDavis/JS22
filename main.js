6aea2f6b08cfbc32213a7edfec09c8d0f7f7289f
Lauren Davis <londond9218@gmail.com>
2015-09-18 11:26:24 -0600

    Star Craft

---
+++ main.js
@@ -0,0 +1,55 @@
+
+var mainControllerFunc = function($scope, starCraftData) {
+	$scope.data = starCraftData; 
+	$scope.sortFrom = '0'; 
+	$scope.showSort = false;
+	$scope.dataFilter = null; 
+	$scope.perPage = 20; 
+	$scope.page = 1; 
+
+
+	$scope.sortData = function(column) {
+		if (column.toString() === $scope.sortFrom) {
+			$scope.showSort = !$scope.showSort;
+		}
+		else {
+			$scope.showSort = false; 
+		}
+		$scope.sortFrom = column.toString();
+	}
+
+	$scope.filterData = function(race) {
+		if (!$scope.dataFilter) {
+			return true;
+		}
+		return race[3] === $scope.dataFilter; 
+
+	}
+
+	$scope.pagination = function(next) {
+		$scope.page += next;
+	}
+
+	$scope.totalPages = Math.ceil($scope.data.data.length / $scope.perPage)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+angular.module("app").controller('mainController', ['$scope', 'starCraftData', mainControllerFunc]);
